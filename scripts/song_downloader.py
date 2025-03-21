83ef2267bab5c5100d906d74769f9aa809e39b04
DeveloperDowny <vedantpanchal12345@gmail.com>
2025-03-21 21:37:23 +0530

    feat: from playlist download option added

--- scripts/song_downloader.py
+++ scripts/song_downloader.py
@@ -9,8 +9,17 @@ class SongDownloader:
             transformed_queries = [f'"{query} song"' for query in transformed_queries]
 
             os.system(f"python song_downloader_cli.py {' '.join(transformed_queries)}")
+    
+    def from_playlist(self, url):
+        from pytubefix import Playlist
+        playlist = Playlist(url)
+        queries = [video.title for video in playlist.videos]
+        transformed_queries = [f'"{query} song"' for query in queries]
+        
+        os.system(f"python song_downloader_cli.py {' '.join(transformed_queries)}")
 
 
 if __name__ == "__main__":
     downloader = SongDownloader()
-    downloader.from_text_file("scripts/data/song-names.txt")
+    # downloader.from_text_file("scripts/data/song-names.txt")
+    downloader.from_playlist("https://music.youtube.com/playlist?list=LRYRAoeXvXhbIW4yo-YABF99bxBQUXb3zOJTD")
